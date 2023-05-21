3c90cb7bdc1230936a04227963f6715dc3740a54
Jahid Shakil <73303787+jahidshakil@users.noreply.github.com>
2023-05-21 18:08:45 +0600

    Time: 169 ms (48.88%), Space: 55.8 MB (59.55%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -27,29 +27,21 @@
 // }
 
 class Solution{
-public int largestRectangleArea(int[] h) {
-  int n = h.length, i = 0, max = 0;
-    
-  Stack<Integer> s = new Stack<>();
-    
-  while (i < n) {
-    // as long as the current bar is shorter than the last one in the stack
-    // we keep popping out the stack and calculate the area based on
-    // the popped bar
-    while (!s.isEmpty() && h[i] < h[s.peek()]) {
-      // tricky part is how to handle the index of the left bound
-      max = Math.max(max, h[s.pop()] * (i - (s.isEmpty() ? 0 : s.peek() + 1)));
+    public int largestRectangleArea(int[] heights) {
+        int len = heights.length;
+        Stack<Integer> s = new Stack<>();
+        int maxArea = 0;
+        for (int i = 0; i <= len; i++){
+            int h = (i == len ? 0 : heights[i]);
+            if (s.isEmpty() || h >= heights[s.peek()]) {
+                s.push(i);
+            } else {
+                int tp = s.pop();
+                maxArea = Math.max(maxArea, heights[tp] * (s.isEmpty() ? i : i - 1 - s.peek()));
+                i--;
+            }
+        }
+        return maxArea;
     }
-    // put current bar's index to the stack
-    s.push(i++);
-  }
-    
-  // finally pop out any bar left in the stack and calculate the area based on it
-  while (!s.isEmpty()) {
-    max = Math.max(max, h[s.pop()] * (n - (s.isEmpty() ? 0 : s.peek() + 1)));
-  }
-    
-  return max;
-}
     
 }
