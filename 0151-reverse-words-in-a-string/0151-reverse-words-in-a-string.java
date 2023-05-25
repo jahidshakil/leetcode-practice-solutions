688edeed8b3527cb55f84d6a675d4eded542ec9a
Jahid Shakil <73303787+jahidshakil@users.noreply.github.com>
2023-05-26 03:47:09 +0600

    Time: 21 ms (8.33%), Space: 45 MB (5.25%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,53 +1,26 @@
 class Solution {
-    // TC : O(N) && SC : O(1)
-    // By using 2 pointer approach (start & end)
-
     public String reverseWords(String s) {
-        int n = s.length(); // length of the string
-        String res = ""; // for storing the result
-        int i = 0; // start pointer
-
-        // this loop runs till end of the string
-        while(i < n){
-            // i was looking for non-sapce
-            // if i found space the this loop terminates, till that this i  will runs
-            while(i < n && s.charAt(i) == ' '){
-                i++;
-            }
-
-            // if i greater than String length, then break
-            // just a check point
-            if(i >= n){
-                break;
-            }
-
-             // j points to next of i, ie; space of next character
-            int j = i + 1;
-
-            // this j loop was looking for space
-            // if space found this while loop terminates and i loop will start
-            while(j < n && s.charAt(j) != ' '){
-                j++;
-            }
-
-            // store the word to sub
-            String sub = s.substring(i, j);
-
-            // if length of res is 0, then store the word to the res
-            if(res.length() == 0){
-                res = sub;
-            } 
-            // if res is not empty don't add
-            else 
-            {
-                // it stores, new sub + " " + earlier sub
-                res = sub + " " + res;
-            }
-            // j loop ends, i loop starts 
-            i = j + 1;
-        }
-
-        // return resultant string
-        return res;
+        Stack<String> st = new Stack<>();
+        String p="";
+     for(int i=0;i<s.length();i++)
+     {
+         if(s.charAt(i)!=' ')
+         {
+             p=p+s.charAt(i);
+         }
+         else
+         {   
+             if(!p.equals("")) 
+             st.push(p);
+             p="";
+         }
+     }      
+        if(!p.equals(""))
+            st.push(p);
+        String ans ="";
+        while(st.size()>1)
+            ans=ans+ st.pop()+" ";
+        ans=ans+st.pop();
+        return ans;
     }
 }
