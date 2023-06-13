e2b1ab7f6a01243efc428a44f365411b05417dfd
Jahid Shakil <73303787+jahidshakil@users.noreply.github.com>
2023-06-13 12:11:28 +0600

    Time: 13 ms (89.06%), Space: 52.8 MB (12.16%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.java
@@ -1,50 +1,50 @@
-// class Solution {
-//     public int equalPairs(int[][] grid) {
-//         int count =0;
-//         int n = grid.length;
-//         int [][]trans =  new int[n][n];
-//         transpose(grid, trans);
-//         for(int i=0; i<grid.length; i++){
-//             for(int j=0; j<grid[i].length; j++){
-//                 if(Arrays.equals(grid[i], trans[j])) count++;
-//             }
-//         }
-//         return count;
-//     }
-    
-    
-    
-//    public static void transpose(int[][]A, int[][]B){
-//         for(int i=0; i<A.length; i++){
-//             for(int j=0; j<A[i].length; j++){
-//                 B[j][i] = A[i][j];
-//             }
-//         }
-//     }
-    
-// }
-
-
 class Solution {
     public int equalPairs(int[][] grid) {
-        HashMap<String, Integer> map = new HashMap<>();
-        int row = grid.length;
-        int col = grid.length;
-        for(int i = 0; i < row; i++){
-            String res = "";
-            for(int j = 0; j < col; j++){
-                res += "-" + grid[i][j];
+        int count =0;
+        int n = grid.length;
+        int [][]trans =  new int[n][n];
+        transpose(grid, trans);
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[i].length; j++){
+                if(Arrays.equals(grid[i], trans[j])) count++;
             }
-            map.put(res, map.getOrDefault(res, 0) + 1);
         }
-        int cnt = 0;
-        for(int j = 0; j < col; j++){
-            String res = "";
-            for(int i = 0; i < row; i++){
-                res += "-" + grid[i][j];
-            }
-            cnt += map.getOrDefault(res, 0);
-        }
-        return cnt;
+        return count;
     }
+    
+    
+    
+   public static void transpose(int[][]A, int[][]B){
+        for(int i=0; i<A.length; i++){
+            for(int j=0; j<A[i].length; j++){
+                B[j][i] = A[i][j];
+            }
+        }
+    }
+    
 }
+
+
+// class Solution {
+//     public int equalPairs(int[][] grid) {
+//         HashMap<String, Integer> map = new HashMap<>();
+//         int row = grid.length;
+//         int col = grid.length;
+//         for(int i = 0; i < row; i++){
+//             String res = "";
+//             for(int j = 0; j < col; j++){
+//                 res += "-" + grid[i][j];
+//             }
+//             map.put(res, map.getOrDefault(res, 0) + 1);
+//         }
+//         int cnt = 0;
+//         for(int j = 0; j < col; j++){
+//             String res = "";
+//             for(int i = 0; i < row; i++){
+//                 res += "-" + grid[i][j];
+//             }
+//             cnt += map.getOrDefault(res, 0);
+//         }
+//         return cnt;
+//     }
+// }
