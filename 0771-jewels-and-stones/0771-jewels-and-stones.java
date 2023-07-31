778413d493e0992a74dc78944e1a011b0742c212
Jahid Shakil <73303787+jahidshakil@users.noreply.github.com>
2023-07-31 13:52:34 +0600

    Time: 1 ms (91.26%), Space: 41.1 MB (26.71%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,14 +1,32 @@
+// class Solution {
+//     public int numJewelsInStones(String jewels, String stones) {
+//         int temp = 0;
+        
+//         for(int i=0; i<jewels.length(); i++){
+//             for(int j=0; j<stones.length(); j++){
+//                 if(jewels.charAt(i) == stones.charAt(j)){
+//                     temp++;
+//                 }
+//             }
+//         }
+//         return temp;
+//     }
+// }
+
+
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int temp = 0;
         
-        for(int i=0; i<jewels.length(); i++){
-            for(int j=0; j<stones.length(); j++){
-                if(jewels.charAt(i) == stones.charAt(j)){
-                    temp++;
-                }
-            }
+        Set st= new HashSet();
+        for(char c : jewels.toCharArray()){
+            st.add(c);
         }
+        
+        for(char c : stones.toCharArray()){
+            if(st.contains(c))temp++;
+        }
+        
         return temp;
     }
 }
