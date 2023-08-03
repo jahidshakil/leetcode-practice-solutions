dfa98fa246b3e59354b15433269d564d2fc6f3a6
Jahid Shakil <73303787+jahidshakil@users.noreply.github.com>
2023-08-03 23:55:48 +0600

    Time: 0 ms (100.00%), Space: 38.8 MB (99.30%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -1,11 +1,37 @@
+// class Solution {
+//     public int kthFactor(int n, int k) {
+//         ArrayList<Integer>arr = new ArrayList<>();
+//         for(int i=1; i<=n; i++){
+//             if(n%i ==0) arr.add(i);
+//         }
+        
+//        if(arr.size() <k) return -1;
+//         else return arr.get(k-1);
+//     }
+// }
+
+
+
+
 class Solution {
     public int kthFactor(int n, int k) {
-        ArrayList<Integer>arr = new ArrayList<>();
-        for(int i=1; i<=n; i++){
-            if(n%i ==0) arr.add(i);
-        }
         
-       if(arr.size() <k) return -1;
-        else return arr.get(k-1);
+//         for(int i=1; i<=(int)Math.sqrt(n); ++i){
+//             if(n%i ==0 && --k==0) return i;
+//         }
+        
+//         for(int i= (int)Math.sqrt(n); i>=1; --i){
+//             if(n%(n/i) ==0 && --k==0) return n/i;
+//         }
+//         return -1;
+        
+        for(int i = 1; i < Math.sqrt(n); ++i) 
+        if(n % i== 0 && --k == 0) 
+            return i;                       
+        for(int i = (int) Math.sqrt(n); i >= 1; --i) 
+            if(n % (n/i) == 0 && --k == 0) 
+                return n/i;          
+        return -1;
+        
     }
 }
