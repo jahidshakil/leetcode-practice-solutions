e4acb753ea7ada34f6de857758f38b8831e34f09
Jahid Shakil <73303787+jahidshakil@users.noreply.github.com>
2023-08-05 11:32:48 +0600

    Time: 52 ms (96.87%), Space: 44.2 MB (89.96%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    var val = init;
+    return {
+        increment : ()=>{
+            return ++val;
+        },
+        decrement: ()=>{
+           val-=1;
+            return val
+    },
+        reset : ()=>{
+            val=init;
+            return val;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
