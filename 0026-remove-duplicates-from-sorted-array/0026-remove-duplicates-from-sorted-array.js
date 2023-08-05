e4e9f544eeff3a17c47a4b5330add4c77a78377f
Jahid Shakil <73303787+jahidshakil@users.noreply.github.com>
2023-08-05 20:24:25 +0600

    Time: 68 ms (74.36%), Space: 44.6 MB (55.43%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -1,14 +1,14 @@
-class Solution {
-    public int removeDuplicates(int[] nums) {
-        int count=0;
-        for(int i=1; i<nums.length; i++){
-            if(nums[i-1] == nums[i]){
-                count++;
-            }else{
-                nums[i-count] = nums[i];
-            }
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let counter=1;
+    for(let i=1;i<nums.length;i++){
+        if(nums[i-1] !== nums[i]){
+            nums[counter]=nums[i];
+            counter++;
         }
-        
-        return nums.length-count;
     }
-}
+    return counter;
+};
