8f92e6ca4f1ee9a76591d29db4a0110e782dacfe
Jahid Shakil <73303787+jahidshakil@users.noreply.github.com>
2023-08-06 11:38:58 +0600

    Time: 54 ms (76.26%), Space: 41.4 MB (95.80%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let counter=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] !== val){
+            nums[counter++]=nums[i];
+        }
+    }
+    return counter;
+};
