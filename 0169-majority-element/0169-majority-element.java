64c88ba012b2fa18df9781d55a8f476934decf4c
Jahid Shakil <73303787+jahidshakil@users.noreply.github.com>
2023-08-06 23:22:03 +0600

    Time: 13 ms (30.08%), Space: 46.3 MB (98.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -6,14 +6,14 @@ class Solution {
         
         for(int i=0; i<nums.length; i++){
             if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i]) + 1);
+                map.put(nums[i], map.get(nums[i])+1);
             }else{
                 map.put(nums[i], 1);
             }
         }
         
-        int ans=0;
-        for(Map.Entry<Integer, Integer>num : map.entrySet()){
+        int ans =0;
+        for(Map.Entry<Integer,Integer>num : map.entrySet()){
             if(num.getValue() > n/2) ans= num.getKey();
         }
         
