ef0ab6612761a53e278544c10d13bd8c60215ade
Jahidul Islam <73303787+jahidshakil@users.noreply.github.com>
2023-09-06 02:08:57 +0600

    Time: 298 ms (60.93%), Space: 107.5 MB (54.67%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    let memo = new Map();
+    return function(...args) {
+        let key = JSON.stringify(args);
+        
+        if(memo.has(key)){
+            return memo.get(key);
+        }else{
+            let ans = fn(...args);
+            memo.set(key, ans);
+            return ans;
+        }
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
