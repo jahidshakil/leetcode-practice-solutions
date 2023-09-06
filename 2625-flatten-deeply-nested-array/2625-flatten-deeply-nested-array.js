99219f47677484341f8a152e157aec6e1e78201e
Jahidul Islam <73303787+jahidshakil@users.noreply.github.com>
2023-09-06 12:44:23 +0600

    Time: 1398 ms (5.20%), Space: 137.1 MB (6.67%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -6,21 +6,18 @@
 var flat = function (arr, n) {
     if(n==0) return arr;
     let q;
-    let curDepth=0
-    let flag = true;
-    while(curDepth<n && flag){
+    while(n>0 ){
          q = [];
-        flag=false;
         for(let i=0; i<arr.length; i++){
             if(Array.isArray(arr[i])){
                 q.push(...arr[i]);
-                flag=true;
+                
             }else{
                 q.push(arr[i]);
             }
         }
         arr= [...q];
-        curDepth++
+        n--
     }
     return arr;
 };
