32194a23d9c8d5abbb9ed5efe29ee3b02dfa3ee5
Jahidul Islam <73303787+jahidshakil@users.noreply.github.com>
2023-09-20 23:45:03 +0600

    Time: 7 ms (43.12%), Space: 41.2 MB (24.88%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,27 +1,44 @@
 class Solution {
+//     public int longestPalindrome(String s) {
+//         Map<Character, Integer>map = new HashMap<>();
+//         for(int i=0; i<s.length(); i++){
+//             if(!map.containsKey(s.charAt(i))){
+//                 map.put(s.charAt(i), 1);
+//             }else{
+//                 map.put( s.charAt(i), map.get(s.charAt(i)) +1);
+//             }
+//         }
+        
+//         int longest = 0;
+//         boolean odd =false;
+//         for(Map.Entry<Character, Integer>mp: map.entrySet()){
+//             if(mp.getValue() % 2==0){
+//                 longest += mp.getValue();
+//             }else{
+//                 odd =true;
+//                 longest+= mp.getValue()-1;
+//             }
+//         }
+        
+//         return odd == true ? longest + 1 : longest;
+        
+        
+//     }
+    
+    
     public int longestPalindrome(String s) {
-        Map<Character, Integer>map = new HashMap<>();
+        if(s==null || s.length()==0) return 0;
+        HashSet<Character> hs = new HashSet<Character>();
+        int count = 0;
         for(int i=0; i<s.length(); i++){
-            if(!map.containsKey(s.charAt(i))){
-                map.put(s.charAt(i), 1);
+            if(hs.contains(s.charAt(i))){
+                hs.remove(s.charAt(i));
+                count++;
             }else{
-                map.put( s.charAt(i), map.get(s.charAt(i)) +1);
+                hs.add(s.charAt(i));
             }
         }
-        
-        int longest = 0;
-        boolean odd =false;
-        for(Map.Entry<Character, Integer>mp: map.entrySet()){
-            if(mp.getValue() % 2==0){
-                longest += mp.getValue();
-            }else{
-                odd =true;
-                longest+= mp.getValue()-1;
-            }
-        }
-        
-        return odd == true ? longest + 1 : longest;
-        
-        
-    }
+        if(!hs.isEmpty()) return count*2+1;
+        return count*2;
+}
 }
