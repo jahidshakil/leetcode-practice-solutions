38e33e4073447c23152ad30a4f54d21d14039f71
Jahidul Islam <73303787+jahidshakil@users.noreply.github.com>
2023-09-23 15:16:56 +0600

    Time: 1 ms (92.37%), Space: 40.4 MB (88.39%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.java
@@ -1,21 +1,40 @@
-class Solution {
-    public boolean areNumbersAscending(String s) {
-        int prev = 0;
-        String arr[] = s.split(" ");
-        
-        for(String str : arr){
-            if(Character.isDigit(str.charAt(0))){
-                int num  = Integer.parseInt(str);
+// class Solution {
+//     public boolean areNumbersAscending(String s) {
+//         int prev = 0;        
+//         for(int i=0; i<s.length(); i++){
+//             if(Character.isDigit(s.charAt(i))){
+//                 int num  = s.charAt(i) - '0';
                 
-                if(num <= prev){
-                    return false;
-                }else{
-                    prev = num;
-                }
-            }
-    }
+//                 if(num <= prev){
+//                     return false;
+//                 }else{
+//                     prev = num;
+//                 }
+//             }
+//     }
         
-        return true; 
-}
+//         return true; 
+// }
     
+// }
+
+class Solution {
+    public boolean areNumbersAscending(String s) 
+    {
+        int prev =0;
+        String[] sent = s.split(" ");
+        
+        for(String i: sent)
+        {
+            if(Character.isDigit(i.charAt(0)) )
+            {
+                int num = Integer.parseInt(i);
+                if(num <= prev)
+                    return false;
+                else
+                    prev = num;
+            }
+        }
+        return true;
+    }
 }
