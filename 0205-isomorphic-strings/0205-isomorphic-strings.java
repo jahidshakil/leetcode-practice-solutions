730a2abbb942adb514ce582dc21d9262fce3904e
Jahidul Islam <73303787+jahidshakil@users.noreply.github.com>
2023-09-23 23:28:18 +0600

    Time: 7 ms (86.38%), Space: 41.1 MB (94.42%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -5,7 +5,7 @@ class Solution {
         int arr2[] = new int[256];
         if(s.length() != t.length()) return false;
         for(int i=0; i<s.length(); i++){
-            if(arr1[ (int)s.charAt(i)] != arr2[ (int)t.charAt(i) ]) return false;
+            if(arr1[ (int)s.charAt(i)] != arr2[(int)t.charAt(i) ]) return false;
             arr1[(int)s.charAt(i) ] =i+1 ;
             arr2[(int)t.charAt(i) ] = i+1;
         }
