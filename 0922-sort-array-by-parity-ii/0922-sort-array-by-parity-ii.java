a879e1c3cf8fb443b10095325cf43f2cb53e9834
Jahidul Islam <73303787+jahidshakil@users.noreply.github.com>
2023-09-24 23:48:16 +0600

    Time: 3 ms (45.50%), Space: 45.1 MB (36.55%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
@@ -1,26 +1,50 @@
 class Solution {
+//     public int[] sortArrayByParityII(int[] nums) {
+//         ArrayList<Integer>temp1 = new ArrayList<>();
+//         ArrayList<Integer>temp2 = new ArrayList<>();
+//         int ans[] = new int[nums.length];
+//         for(int i=0; i<nums.length; i++){
+//             if( nums[i] % 2== 0){
+//                 temp1.add(nums[i]);
+//             }else{
+//                 temp2.add(nums[i]);
+//             }
+//         }
+        
+//         for(int i=0; i<nums.length; i++){
+//             if(i %2 ==0){
+//                 ans[i] = temp1.remove(0);
+//             }else{
+//                 ans[i] = temp2.remove(0);
+//             }
+//         }
+        
+//         return ans;
+        
+        
+//     }
+    
     public int[] sortArrayByParityII(int[] nums) {
-        ArrayList<Integer>temp1 = new ArrayList<>();
-        ArrayList<Integer>temp2 = new ArrayList<>();
-        int ans[] = new int[nums.length];
-        for(int i=0; i<nums.length; i++){
-            if( nums[i] % 2== 0){
-                temp1.add(nums[i]);
-            }else{
-                temp2.add(nums[i]);
+        int even = 0;
+        int odd = 1;
+        
+        while(even < nums.length && odd < nums.length) {
+            if(nums[even]%2 != 0) {
+                swap(nums, even, odd);
+                odd += 2;
+            } else {
+                even += 2;
             }
         }
-        
-        for(int i=0; i<nums.length; i++){
-            if(i %2 ==0){
-                ans[i] = temp1.remove(0);
-            }else{
-                ans[i] = temp2.remove(0);
-            }
-        }
-        
-        return ans;
-        
-        
+
+        return nums;
+    }
+    
+    
+    public void swap(int[]nums, int l, int r  ){
+        int temp = nums[r];
+        nums[r] = nums[l];
+        nums[l] = temp;
     }
 }
+
