b5afdf320f89ac094d00967af7d4c6b28ee7a573
Jahidul Islam <73303787+jahidshakil@users.noreply.github.com>
2023-09-27 00:10:28 +0600

    Time: 0 ms (100.00%), Space: 40.2 MB (57.54%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
@@ -1,20 +1,35 @@
+// class Solution {
+//     public int dominantIndex(int[] nums) {
+//         int largestNum = 0;
+//         int largestIndex = 0;
+//         for(int i =0; i<nums.length; i++){
+//             if(nums[i] > largestNum){
+//                 largestNum = nums[i];
+//                 largestIndex = i;
+//             }
+//         }
+        
+//         for(int i =0; i<nums.length; i++){
+//             if( i != largestIndex && largestNum < 2*nums[i]){
+//                 return -1;
+//             }
+//         }
+        
+//         return largestIndex;
+//     }
+// }
+
 class Solution {
     public int dominantIndex(int[] nums) {
-        int largestNum = 0;
-        int largestIndex = 0;
-        for(int i =0; i<nums.length; i++){
-            if(nums[i] > largestNum){
-                largestNum = nums[i];
-                largestIndex = i;
-            }
+        int max = -1, index = -1, second = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                second = max;
+                max = nums[i];
+                index = i;
+            } else if (nums[i] > second)
+                second = nums[i];
         }
-        
-        for(int i =0; i<nums.length; i++){
-            if( i != largestIndex && largestNum < 2*nums[i]){
-                return -1;
-            }
-        }
-        
-        return largestIndex;
+        return second * 2 <= max ? index : -1;
     }
 }
