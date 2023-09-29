2d99d6b2e027de4055ebdc8ec1ad8c230e2efedf
Jahidul Islam <73303787+jahidshakil@users.noreply.github.com>
2023-09-29 22:41:34 +0600

    Time: 0 ms (100.00%), Space: 40.8 MB (59.45%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.java
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -1,19 +1,34 @@
-class Solution {
+// class Solution {
+//     public String truncateSentence(String s, int k) {
+//         String arr[] = s.split(" ");
+//         StringBuilder sb = new StringBuilder("");
+        
+//         int i=0;
+//         while(i < k){
+//             if(i==0){
+//                 sb.append(arr[i]);
+//                 i++;
+//             }else{
+//                 sb.append(" " + arr[i]);
+//                 i++;
+//             }
+//         }
+        
+//         return sb.toString();
+//     }
+// }
+
+
+class Solution{
     public String truncateSentence(String s, int k) {
-        String arr[] = s.split(" ");
-        StringBuilder sb = new StringBuilder("");
+        int idx=0;
+        int spaceCount=0;
         
-        int i=0;
-        while(i < k){
-            if(i==0){
-                sb.append(arr[i]);
-                i++;
-            }else{
-                sb.append(" " + arr[i]);
-                i++;
-            }
+        while(idx<s.length() && spaceCount<k){
+            if(s.charAt(idx)==' ') spaceCount++;
+            idx++;
         }
-        
-        return sb.toString();
+        // if(spaceCount<k) means we have to include whole string 
+        return spaceCount==k?s.substring(0,idx-1):s;
     }
 }
